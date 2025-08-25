14e7d8ca3ae42cd533800c8a184494e5f2ce81cd
Tuan Le Thanh <tuanitpro@gmail.com>
2025-08-25 12:15:04 +0700

    Update

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import type { ReactElement } from "react";
+import "./index.css";
+
+type Props = {
+  title?: string;
+  open?: boolean;
+  onClose?: () => void;
+  children: ReactElement;
+};
+
+const Modal = ({ title, open, onClose, children }: Props) => {
+  return !open ? null : (
+    <>
+      <label htmlFor="modal" className="modal-background"></label>
+      <div className="modal">
+        <div className="modal-header">
+          <h3>{title}</h3>
+          <label htmlFor="modal" onClick={() => onClose?.()}>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAdVBMVEUAAABNTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU1NTU0N3NIOAAAAJnRSTlMAAQIDBAUGBwgRFRYZGiEjQ3l7hYaqtLm8vsDFx87a4uvv8fP1+bbY9ZEAAAB8SURBVBhXXY5LFoJAAMOCIP4VBRXEv5j7H9HFDOizu2TRFljedgCQHeocWHVaAWStXnKyl2oVWI+kd1XLvFV1D7Ng3qrWKYMZ+MdEhk3gbhw59KvlH0eTnf2mgiRwvQ7NW6aqNmncukKhnvo/zzlQ2PR/HgsAJkncH6XwAcr0FUY5BVeFAAAAAElFTkSuQmCC"
+              width="16"
+              height="16"
+              alt=""
+            />
+          </label>
+        </div>
+        <div className="modal-content">{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,17 +1,23 @@
 import PageLayout from "@layouts/PageLayout";
 
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { gql, request } from "graphql-request";
 import { useMediaQuery } from "react-responsive";
+import Modal from "@components/Modal";
 
 const Home = () => {
   const title = "Tuấn - Hãy theo đuổi đam mê, nợ nần sẽ theo đuổi bạn";
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [post, setPost] = useState<any>(undefined);
 
   const postQuery = gql`
     {
       posts {
         nodes {
           excerpt
+          content
           id
           title
           slug
@@ -44,6 +50,14 @@ const Home = () => {
           </div>
         </article>
       </header>
+
+      <Modal title={post?.title} open={open} onClose={() => setOpen(false)}>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: post?.content,
+          }}
+        />
+      </Modal>
       <article className="hentry">
         <div className={isMobile ? "" : "entry-content"}>
           {isPending && "Loading..."}
@@ -67,7 +81,15 @@ const Home = () => {
                       />
                     </div>
                   )}
-                  <a href={post?.link}>{post.title}</a>
+                  <a
+                    onClick={() => {
+                      setOpen(true);
+                      setPost(post);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {post.title}
+                  </a>
                   <div
                     className="description"
                     dangerouslySetInnerHTML={{
