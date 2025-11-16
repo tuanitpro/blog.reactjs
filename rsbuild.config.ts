9e6454628f6b440eb27a0c6531e74c77f9e567ec
Tuan Le Thanh <tuanitpro@gmail.com>
2025-11-16 10:58:59 +0700

    Update

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,14 +6,9 @@ import path from "path";
 const { publicVars, rawPublicVars } = loadEnv({ prefixes: ['REACT_APP_'] })
 export default defineConfig({
   html: {
+    template: path.resolve(__dirname, 'public', 'index.html'),
     title: "Tuấn - Hãy theo đuổi đam mê, nợ nần sẽ theo đuổi bạn",
     favicon: "./src/favicon.ico",
-    meta: {
-      charset: {
-        charset: "UTF-8",
-      },
-      viewport: "width=device-width, initial-scale=1.0",
-    },
   },
   source: {
     define: {
